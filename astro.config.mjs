58071a5c63e62e54841d8170172f1f27906a0574
Federico Maria Morrone <contact@morrone.dev>
2024-03-06 16:16:18 +0100

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://cake.morrone.dev",
+  integrations: [tailwind(), sitemap()]
+});
