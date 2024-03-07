2816e6cc63df9b1ad1e71ef32a5b1784f7b56961
Matilde Morrone <contact@morrone.dev>
2024-03-08 00:07:36 +0100

    Use basic css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			colors: {
-				'credits-yellow': '#ffb600',
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 }
