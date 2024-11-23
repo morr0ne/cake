590abe4feb35d852ca17cb0d8b4d3a21ed1de8a8
Matilde Morrone <contact@morrone.dev>
2024-11-23 17:31:05 +0100

    Use typescript for astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cake.morrone.dev",
-    integrations: [tailwind(), sitemap()]
+  site: "https://cake.morrone.dev",
+  integrations: [tailwind(), sitemap()],
 });
-
