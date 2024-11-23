ab233ab77e138e736af7027ec13866ea248e6ea4
Matilde Morrone <contact@morrone.dev>
2024-11-23 17:37:33 +0100

    Implement formatting via prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
