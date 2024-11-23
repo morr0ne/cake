8731a9689c2450802d6d26c3296c91b40d06408f
Federico Maria Morrone <contact@morrone.dev>
2024-11-23 17:37:33 +0100

    Implement formatting via prettier

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cake.morrone.dev",
+  site: 'https://cake.morrone.dev',
   integrations: [tailwind(), sitemap()],
 });
